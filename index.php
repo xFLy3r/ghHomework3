fd9e4cfb3468a414f32e583fe1b45519974fdbd1
xFLy3r <datus3r@gmail.com>
2016-10-30 17:58:38 +0200

    Created instances of classes

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$realnameGenerator = new \Vendor\Generator\Name\RealnameGenerator('Male');
+$passwordGenerator = new \Vendor\Generator\Password\PasswordGenerator();
